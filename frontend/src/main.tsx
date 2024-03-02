f6645e25d96befc7cf1c5e3dbafab4e4908a883d
b-sharman <byron.n.sharman@gmail.com>
2024-03-02 16:30:09 -0700

    add routing

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from './routes/root.tsx'
+import SignupPage from './routes/signup/signup.tsx'
+import LoginPage from './routes/login/login.tsx'
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
--- frontend/src/routes/root.tsx
+++ frontend/src/routes/root.tsx
@@ -1,9 +1,9 @@
 import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query';
-import QueryApp from './QueryApp.tsx'
+import QueryApp from '../QueryApp.tsx'
 
 const queryClient = new QueryClient();
 
-export default function App() {
+export default function Root() {
 
    return (
      <QueryClientProvider client={queryClient}>
