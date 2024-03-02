9d3a605db5ba5100da547331693dbf5725d80de9
b-sharman <byron.n.sharman@gmail.com>
2024-03-02 15:16:01 -0700

    add window controls

---
+++ frontend/src/Wobject.tsx
@@ -0,0 +1,22 @@
+function Wobject({ Child }) {
+  return (
+    <div className="border rounded-2xl">
+      <navbar className="w-full flex flex-row place-content-between p-1 border-b">
+        {/* drag handle */}
+        <div className="w-full flex flex-row place-content-center cursor-move">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+            <path fillRule="evenodd" d="M4.5 12a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Zm6 0a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Zm6 0a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Z" clipRule="evenodd" />
+          </svg>
+        </div>
+
+        {/* close button */}
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5 m-auto">
+          <path fillRule="evenodd" d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
+        </svg>
+      </navbar>
+      <Child />
+    </div>
+  )
+}
+
+export default Wobject
