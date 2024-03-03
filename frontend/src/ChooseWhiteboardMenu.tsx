14083505fac740e648dc18a78a372b3e88f38c17
Tyler Wright <tyler.wright5146@gmail.com>
2024-03-03 11:27:33 -0700

    global search

--- frontend/src/ChooseWhiteboardMenu.tsx
+++ frontend/src/ChooseWhiteboardMenu.tsx
@@ -15,8 +15,60 @@ interface ChooseWhiteboardMenuProps {
     api: Api;
 }
 
+interface GlobalSearchResult {
+    [key: number]: number;
+}
+
+function debounce(func: (...args: any[]) => void, wait: number): (...args: any[]) => void {
+    let timeout: ReturnType<typeof setTimeout> | null;
+    return function executedFunction(...args: any[]): void {
+      const later = () => {
+        clearTimeout(timeout!);
+        func(...args);
+      };
+      clearTimeout(timeout!);
+      timeout = setTimeout(later, wait);
+    };
+  }
+
 function ChooseWhiteboardMenu(props: ChooseWhiteboardMenuProps) {
     const [loadedWhiteboards, setLoadedWhiteboards] = useState<WhiteboardMetadataGET[]>([])
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const filterAndSortWhiteboards = async (searchTerm: string) => {
+        if (searchTerm === '') {
+          return;
+        }
+    
+        const results = await fetch(`${import.meta.env.VITE_BASE_URL}/global_search`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          credentials: 'include',
+          body: JSON.stringify({
+            query: searchTerm
+          })
+        });
+    
+        const res: GlobalSearchResult[] = await results.json();
+
+        const temp: WhiteboardMetadataGET[] = [];
+    
+        for (let i = 0; i < res.length; i++) {
+          const id = parseInt(Object.keys(res[i])[0]);
+          console.log(id)
+          temp.push(loadedWhiteboards.find((whiteboard) => whiteboard.id === id)!);
+        }
+    
+        setLoadedWhiteboards(temp);
+      };
+
+    const debouncedFilterAndSort = debounce(filterAndSortWhiteboards, 1000); // 300ms delay
+
+    useEffect(() => {
+        debouncedFilterAndSort(searchTerm);
+      }, [searchTerm]);    
 
     useEffect(() => {
         if (props.needToReload) {
@@ -39,6 +91,16 @@ function ChooseWhiteboardMenu(props: ChooseWhiteboardMenuProps) {
     return (
         <>
             <ul className="flex flex-col gap-2">
+                <li className="px-4 py-2 cursor-pointer transition rounded-lg">
+                    <input
+                        name="search"
+                        type="text"
+                        placeholder="Search"
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                        className="w-full rounded-full p-3"
+                    />
+                </li>
                 {loadedWhiteboards.map((whiteboard, i) => (
                     <li
                         key={i}
--- frontend/src/HamburgerMenu.tsx
+++ frontend/src/HamburgerMenu.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import WhiteboardItem from './WhiteboardItem';
 import { WhiteboardMetadataGET } from './api/ApiTypes';
 import './HamburgerMenu.css';
@@ -10,8 +10,25 @@ interface HamburgerMenuProps {
   setSelectedWhiteboardID: (arg0: number) => void;
 }
 
+interface GlobalSearchResult {
+  [key: string]: string;
+}
+
+function debounce(func: (...args: any[]) => void, wait: number): (...args: any[]) => void {
+  let timeout: ReturnType<typeof setTimeout> | null;
+  return function executedFunction(...args: any[]): void {
+    const later = () => {
+      clearTimeout(timeout!);
+      func(...args);
+    };
+    clearTimeout(timeout!);
+    timeout = setTimeout(later, wait);
+  };
+}
+
 function HamburgerMenu({ WhiteboardMetadataGETs, setIsPopupOpen, setSelectedWhiteboardID }: HamburgerMenuProps) {
   const [isOpen, setIsOpen] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
@@ -21,6 +38,41 @@ function HamburgerMenu({ WhiteboardMetadataGETs, setIsPopupOpen, setSelectedWhit
     setIsPopupOpen(true);
   };
 
+  const filterAndSortWhiteboards = async (searchTerm: string) => {
+    if (searchTerm === '') {
+      WhiteboardMetadataGETs = WhiteboardMetadataGETs.sort((a, b) => a.name.localeCompare(b.name));
+      return;
+    }
+
+    const results = await fetch(`${import.meta.env.VITE_BASE_URL}/global_search`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      credentials: 'include',
+      body: JSON.stringify({
+        query: searchTerm
+      })
+    });
+
+    const res: GlobalSearchResult[] = await results.json();
+
+    const temp: WhiteboardMetadataGET[] = [];
+
+    for (let i = 0; i < res.length; i++) {
+      const id = parseInt(res[i][Object.keys(res[i])[0]]);
+      temp.push(WhiteboardMetadataGETs.find((whiteboard) => whiteboard.id === id)!);
+    }
+
+    WhiteboardMetadataGETs = temp;
+  };
+
+  const debouncedFilterAndSort = debounce(filterAndSortWhiteboards, 1000); // 300ms delay
+
+  useEffect(() => {
+    debouncedFilterAndSort(searchTerm);
+  }, [searchTerm]);
+
   return (
     <div className="app">
       <div className={`menu-toggle ${isOpen ? 'open' : ''} z-[100]`} onClick={toggleMenu}>
@@ -28,6 +80,7 @@ function HamburgerMenu({ WhiteboardMetadataGETs, setIsPopupOpen, setSelectedWhit
           <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
         </svg>
       </div>
+      
       <div className={`menu ${isOpen ? 'open' : ''} z-[9999] flex flex-col place-content-between bg-gray-100 p-2 pt-16`}>
         <ul className="flex flex-col gap-2">
           {WhiteboardMetadataGETs.map(item => (
