627d29530e071b25d563d6639bd002a65f95c811
b-sharman <byron.n.sharman@gmail.com>
2024-03-03 03:24:23 -0700

    new whiteboard dialog styles part 0 (of n>0)

--- frontend/src/QueryApp.tsx
+++ frontend/src/QueryApp.tsx
@@ -58,14 +58,19 @@ function QueryApp() {
   return (
     <div>
       {isPopupOpen && (
-        <dialog open>
-          <h2>Enter the whiteboard name:</h2>
-          <form onSubmit={makeWhiteboardOfName}>
-            <input type="text" name="whiteboardName" />
-            <button type="submit">Save</button>
-            <button onClick={closePopup}>Cancel</button>
-          </form>
-        </dialog>
+        /* to darken background, need to have element covering entire screen with transparent black background */
+        <div className="fixed w-full h-full bg-black/35 z-40">
+          <dialog open className="fixed inset-0 p-4 rounded-xl">
+            <form onSubmit={makeWhiteboardOfName}>
+              <h2 className="font-bold text-center">New Whiteboard</h2>
+              <input type="text" name="whiteboardName" className="block m-3 px-4 py-2 rounded-full bg-50 outline outline-1 outline-gray-200 focus:outline-none focus:ring focus:ring-blue-600" placeholder="Type a whiteboard name" autofocus />
+              <footer className="flex flex-row gap-2 basis-full">
+                <button onClick={closePopup} className="grow p-2 rounded-lg bg-gray-100">Cancel</button>
+                <button type="submit" className="grow p-2 rounded-lg bg-blue-600 text-gray-50">Save</button>
+              </footer>
+            </form>
+          </dialog>
+        </div>
       )}
       {data.length == 0
         ?
