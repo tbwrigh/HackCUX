7e3973f9a87670b391839e0a39e8dbd31dce95e1
Goldey2 <ryyanmapes@gmail.com>
2024-03-02 18:43:41 -0700

    including session info when fetching whiteboard metadata

--- frontend/src/QueryApp.tsx
+++ frontend/src/QueryApp.tsx
@@ -7,8 +7,20 @@ import Whiteboard from "./Whiteboard.tsx"
 import { useQuery, QueryKey, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { WhiteboardMetadata } from './WhiteboardMetadata.ts';
 import ToolsSidebar from './ToolsSidebar.tsx'
+import { useCookies } from 'react-cookie';
+
+type Cookie = {
+  session_id: string;
+};
 
 function QueryApp() {
+
+  const [cookies, setCookie] = useCookies(['session_id']);
+  const session_id = cookies.session_id;
+  
+  const headers = new Headers();
+  headers.append('Content-Type', 'application/json');
+  headers.append('Cookie', 'session_id='+session_id);
 
   const [count, setCount] = useState(0)
   const { isLoading, isError, data: whiteboardMetadatas } = useQuery({
